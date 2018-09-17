12e12a7ac1e6e04fa2efb51fd56c4a205fa40a53
Ekaterina911 <sos91195@mail.ru>
2018-09-17 10:33:09 +0300

    Task 1_2

--- Laboratory-work-1/Laboratory-work-1/Program.cs
+++ Laboratory-work-1/Laboratory-work-1/Program.cs
@@ -6,17 +6,13 @@ namespace Laboratory_work_1
     {
         static void Main(string[] args)
         {
+            //Task 1_1
+            #region 
             // Посчитаем отдельно числитель и знаменатель для того чтобы исключить 0
 
             double Y = 0.0, Numerator = 0.0, Denominator = 0.0;     //Переменные, Numerator - числитель, Denominator - знаменатель 
             Console.WriteLine("Введите Х из предложенных вариантов: -5,-2,2,5");
             int X = Convert.ToInt32(Console.ReadLine());
-
-            /*                                                      //Преобразование через TryParse
-            string S = Console.ReadLine();
-            int X;
-            if (Int32.TryParse (S, out X))
-            */
 
             Numerator = 2 * Math.Pow(X, 3) + 6 * Math.Pow(X, 2) - 8 * X + 4;
             Denominator = ((-4) * Math.Pow(X, 3)) + (8 * Math.Pow(X, 2)) - (Math.Pow(X, 5)) + 2 * Math.Pow(X, 4);
@@ -28,6 +24,23 @@ namespace Laboratory_work_1
                 Y = Numerator / Denominator;
                 Console.Write("Y = {0, 6:F3}", Y);
             }
+            #endregion
+
+            //Task 1_2
+            #region
+            double y = 0.0, a = 1.5, x;
+            Console.WriteLine("Выберете x из предложенных вариантов:0.001, 0.1, 1, 4");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            y = (Math.Pow(a, x) * Math.Pow(x, a) + 
+                2 * Math.Pow(a, 2 * x) * Math.Pow(x, 2 * a) - 
+                2 * Math.Pow(a, x) * Math.Pow(x, 2 * a) - 
+                4 * Math.Pow(a, 2 * x) * Math.Pow(x, 2 * a)) / 
+                (Math.Log10(a) + Math.Log10(x));
+
+            Console.WriteLine("y= {0,6:F3}", y);
+            #endregion
+
             Console.ReadKey();
         }
     }
