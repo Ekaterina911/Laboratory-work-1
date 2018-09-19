863053fc312e53246b0db0d8fbd248360eefd12b
Ekaterina911 <sos91195@mail.ru>
2018-09-19 15:16:38 +0300

    add class

---
+++ Laboratory-work-1/Laboratory-work-1/Task/Task1_5.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Laboratory_work_1
+{
+    public class Task1_5 : ITask
+    {
+        private double X { get; set; }
+        private readonly int A = 2;
+
+        public Task1_5(double X)
+        {
+            this.X = X;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Y = {ResultTask()}");
+        }
+
+        public double ResultTask()
+        {
+            double d = Denominator(X);
+            double n = Numerator(X);
+            if (d == 0)
+                return 0;
+            else return n / d;
+        }
+
+        public double Numerator(double X)
+        {
+            return Math.Pow(A, Math.Pow(X, A)) *
+                   Math.Pow(X, Math.Pow(A, A * X));
+        }
+
+        public double Denominator(double X)
+        {
+            return Math.Pow(A, A * X) + 
+                   Math.Pow(X, A * X);
+        }
+    }
+}
